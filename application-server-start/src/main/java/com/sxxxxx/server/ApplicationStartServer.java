022cbfac28ea3a07620ce46ddca349211a625471
sx <si.xiang2@iwhalecloud.com>
2023-11-03 14:10:08 +0800

    增加启动类

---
+++ application-server-start/src/main/java/com/sxxxxx/server/ApplicationStartServer.java
@@ -0,0 +1,13 @@
+package com.sxxxxx.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationStartServer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStartServer.class, args);
+    }
+
+}
